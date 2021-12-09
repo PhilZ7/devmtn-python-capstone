a54d4d41ce84d1a35f26b3a836464005994d180e
PhilZ7 <phil84_@hotmail.com>
2021-12-09 12:02:19 -0600

    file structure and updates to css

--- foodtracker/main/routes.py
+++ foodtracker/main/routes.py
@@ -4,12 +4,12 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
-    return render_template('/index.html')
+    return render_template('/templates/index.html')
 
 @main.route('/add')
 def add():
-    return render_template('/add.html')
+    return render_template('/templates/add.html')
 
 @main.route('/view')
 def view():
-    return render_template('/view.html')
+    return render_template('/templates/view.html')
